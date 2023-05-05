a45f965a9b5ccc79840fdf3e79033092508b8c11
Paul Harrison <paul.harrison@manchester.ac.uk>
2023-05-05 12:37:30 +0100

    initial import
    vo-dml files are as found in repositories

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Data Model Playground
+=====================
+
+This repository does not contain the "official versions" of the data models, 
+but gathers them together to make it easy to experiment with various 
+refactorings.
+
+It uses the [vodml tools](https://github.com/ivoa/vo-dml/blob/master/tools/ReadMe.md) and has the original source for the models as [vodsl](https://www.ivoa.net/documents/Notes/VODSL/index.html) in the [models](./models) directory.
---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("net.ivoa.vo-dml.vodmltools") version "0.3.19"
+    application
+}
+
+vodml {
+    vodmlDir.set(file("vo-dml"))
+    vodslDir.set(file("model"))
+    bindingFiles.setFrom(file("vo-dml/*.vodml-binding.xml")
+    )
+
+}
+/* uncomment below to run the generation of vodml from vodsl automatically */
+//tasks.named("vodmlJavaGenerate") {
+//    dependsOn("vodslToVodml")
+//}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
+
+    testImplementation("org.apache.derby:derby:10.14.2.0")
+    testImplementation("org.javastro:jaxbjpa-utils:0.1.2")
+    testImplementation("org.javastro:jaxbjpa-utils:0.1.2:test")
+}
+
+
+application {
+    mainClass.set("Genschema")
+}
